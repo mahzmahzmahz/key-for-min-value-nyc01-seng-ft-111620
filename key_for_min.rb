2cf1b14cbbcbb2d65fd78c6b45b768364c43fd86
Alex Marz <alexhmarz@gmail.com>
2020-11-04 21:03:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,21 +3,21 @@
 
 
 def key_for_min_value(name_hash)
-minimum_value = nil 
-minimum_key = nil
-  name_hash.each do |k, v|
-    if minimum_value == nil
-      minimum_value = v
-      minimum_key = k 
+key = nil 
+value = nil
+  name_hash.each do |kid, age|
+    if value == nil
+      value = age
+      key = kid 
     elsif
-      minimum_value > v
-      minimum_value = v
-      minimum_key = k
+      value > age
+      value = age
+      key = kid
     end
      
   end
  
-  minimum_key
+key
     
 
 end
