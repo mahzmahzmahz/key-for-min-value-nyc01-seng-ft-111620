f383eda6aacd513eebb6297d722c207e794fab78
Alex Marz <alexhmarz@gmail.com>
2020-11-04 20:57:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,17 @@ minimum_key = nil
 
 def key_for_min_value(name_hash)
   name_hash.each do |key, value|
-    if 
+    if minimum_value == nil
+      minimum_value = value
+      minimum_key = key 
+    else
+      minimum_value > value
+      minimum_value = value
+      minimum_key = key
+    end
+  end
+  minimum_key
+  
     
 
 end
